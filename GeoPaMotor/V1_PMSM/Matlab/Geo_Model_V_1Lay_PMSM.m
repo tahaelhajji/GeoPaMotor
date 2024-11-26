9d1b0a7945086998ec34c05cb6a28506f3ee747c
Taha El Hajji <38730694+tahaelhajji@users.noreply.github.com>
2024-11-26 18:32:06 +0000

    Delete GeoPaMotor/V1_PMSM/Matlab directory

--- GeoPaMotor/V1_PMSM/Matlab/Geo_Model_V_1Lay_PMSM.m
+++
@@ -1,5 +0,0 @@
-[Geo_Model] = Geo_Model_V_1Lay_PMSM(Input)
-
-    % This function will be provided in the next release
-
-end
--- GeoPaMotor/V1_PMSM/Matlab/Get_Norm_Bd_Param.m
+++
@@ -1,7 +0,0 @@
-function [Normal_Bd_Geo_Param_pop_i,Normal_Bd_Supp_Param_pop_i]=Get_Norm_Bd_Param(Normal_Param_pop_i)
-
-
-    % This function will be provided in the next release
-
-
-end
